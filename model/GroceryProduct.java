11fcb2f3fa0afb8cc85544e0a0a72dbdf7d71e7e
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-15 18:03:33 +0500

    Add GroceryProduct class with expiry date and tax calculation

--- model/GroceryProduct.java
+++ model/GroceryProduct.java
@@ -1,1 +1,18 @@
 
+package model;
+
+public class GroceryProduct extends AbstractProduct {
+    private String expiryDate;
+
+    public GroceryProduct(String id, String name, double price, int quantity, String expiryDate) {
+        super(id, name, price, quantity);
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public double calculateTax() {
+        return getPrice() * 0.05; 
+    }
+
+    public String getExpiryDate() { return expiryDate; }
+}
