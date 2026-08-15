3880ecc0e9378fce312b04d8d9944351dbca8e2a
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-15 18:02:20 +0500

    Add AbstractProduct class with properties and methods

--- model/AbstractProduct.java
+++ model/AbstractProduct.java
@@ -1,1 +1,31 @@
+package model;
 
+public abstract class AbstractProduct {
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public AbstractProduct(String id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public abstract double calculateTax();
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s | Price: $%.2f | Stock: %d", id, name, price, quantity);
+    }
+}
