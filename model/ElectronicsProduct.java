9dea01d32d6eb7ebbc46d9e273b7cce1ea8cfe8f
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-15 18:02:55 +0500

    Add ElectronicsProduct class with warranty and tax calculation

--- model/ElectronicsProduct.java
+++ model/ElectronicsProduct.java
@@ -1,1 +1,18 @@
 
+package model;
+
+public class ElectronicsProduct extends AbstractProduct {
+    private int warrantyMonths;
+
+    public ElectronicsProduct(String id, String name, double price, int quantity, int warrantyMonths) {
+        super(id, name, price, quantity);
+        this.warrantyMonths = warrantyMonths;
+    }
+
+    @Override
+    public double calculateTax() {
+        return getPrice() * 0.15; 
+    }
+
+    public int getWarrantyMonths() { return warrantyMonths; }
+}
