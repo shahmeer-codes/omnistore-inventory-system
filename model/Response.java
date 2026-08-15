719e84a12c9c60463faa84a8ba37108da5b58e8d
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-15 18:05:20 +0500

    Add generic Response class for API responses

--- model/Response.java
+++ model/Response.java
@@ -1,1 +1,18 @@
 
+package model;
+
+public class Response<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public Response(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() { return success; }
+    public String getMessage() { return message; }
+    public T getData() { return data; }
+}
