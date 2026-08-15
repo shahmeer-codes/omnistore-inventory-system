7be2cd33503fc1c1171743f107ca42fba8c010aa
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-15 18:07:24 +0500

    Implement logging functionality in Logger class

--- util/Logger.java
+++ util/Logger.java
@@ -1,1 +1,15 @@
 
+package util;
+
+import java.util.Date;
+
+public class Logger {
+    public static void log(String level, String... messages) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[").append(new Date()).append("] [").append(level).append("]: ");
+        for (String msg : messages) {
+            sb.append(msg).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
